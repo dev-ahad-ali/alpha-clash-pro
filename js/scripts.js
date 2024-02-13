a69df50aa90f92c3af14bbae00af3cb0b1257a6c
Ahad Ali <ahadali.dev@gmail.com>
2024-02-13 23:27:42 +0600

    hide show function and random alphabet function added

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function continueGame() {
+  const alphabet = getARandomAlphabet();
+}
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
+}
---
+++ js/utility.js
@@ -0,0 +1,22 @@
+function hideElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
+}
+
+function showElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+
+function getARandomAlphabet() {
+  // get alphabet
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
+
+  // get random number between 0 - 25
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+
+  const alphabet = alphabets[index];
+  return alphabet;
+}
